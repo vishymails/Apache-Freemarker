e5c80f7165f75b57c312dba1942a8f53deae526b
vishymails <vishymails@gmail.com>
2019-10-14 12:23:48 +0530

    Add files via upload

---
+++ ValueExampleObject.java
@@ -0,0 +1,22 @@
+package basictemplate;
+
+public class ValueExampleObject {
+
+    private String name;
+    private String developer;
+
+    public ValueExampleObject(String name, String developer) {
+        this.name = name;
+        this.developer = developer;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDeveloper() {
+        return developer;
+    }
+
+}
+
